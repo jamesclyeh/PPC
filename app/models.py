effd027300c4cf7a97c58470a57b9cb05f6a22cd
JamesCLY <jamesclyeh@gmail.com>
2013-07-03 16:51:41 -0400

    Add user schema.

--- app/models.py
+++ app/models.py
@@ -1,14 +1,13 @@
 from app import db
 
-class CareGiver(db.Model):
+CARE_GIVER = 0
+PATIENT = 1
+class User(db.Model):
   id = db.Column(db.Integer, primary_key = True)
   name = db.Column(db.String(64), index = True, unique = True)
-  email = db.Column(db.String(120), index = True, unique = True)
-
-class Patient(db.Model):
-  id = db.Column(db.Integer, primary_key = True)
-  nickname = db.Column(db.String(64), index = True, unique = True)
-  email = db.Column(db.String(120), index = True, unique = True)
+  role = db.Column(db.Integer)
+  email = db.Column(db.String(120), unique = True)
+  phone_number = db.Column(db.String(32))
 
   def __repr__(self):
-    return '<User %r>' % (self.nickname)
+    return '<User %r>' % (self.name)
--- hello.py
+++ hello.py
@@ -3,11 +3,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL',
+    'postgresql+psycopg2://awvolathokychi:SfaeRpVGxZowm_K7IDkTGvEvcM' +
+    '@ec2-23-23-95-222.compute-1.amazonaws.com' +
+    ':5432/d6p5qsqchkuarn')
 
 @app.route('/')
 def hello():
-  return render_template(app.root_path + '/static/form/form.html')
+  return 'Hello World'#render_template(app.root_path + '/static/form/form.html')
 @app.route('/initDb')
 def initDb():
   db = SQLAlchemy(app)
+  return None
