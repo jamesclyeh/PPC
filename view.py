0d1e4f4623d482c8c9ed0df8e0c5eff8da9cc5b5
Jonathan Lam <jonathan.g.lam@gmail.com>
2013-07-22 13:15:38 -0400

    fixing the update inventory on prescription update

--- view.py
+++ view.py
@@ -50,7 +50,7 @@ def updateInventory():
   if len(records) > 0:
     latest_record = records[0]
   current_inventory = latest_record.inventory if latest_record else 0
-  new_amount = current_inventory + int(refill_amount) if int(refill_amount) else None
+  new_amount = current_inventory + int(refill_amount) if refill_amount else None
   if latest_record:
     new_amount = current_inventory - prescription.dosage
   print new_amount
@@ -81,7 +81,6 @@ def updatePrescription(prescription_id):
   record = models.Prescription.query.get(prescription_id)
   for key, value in request.json.iteritems():
     setattr(record, key, value)
-  db.session.save(record)
   db.session.commit()
   return Response('', status=200)
 
